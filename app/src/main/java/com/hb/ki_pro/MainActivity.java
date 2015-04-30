37ef09b30f3b0237b6d60b0783fc80e2007562fd
wlyoung89 <wndehdls@naver.com>
2015-04-30 17:52:19 +0900

    2nd

--- app/src/main/java/com/hb/ki_pro/MainActivity.java
+++ app/src/main/java/com/hb/ki_pro/MainActivity.java
@@ -1,13 +1,13 @@
 package com.hb.ki_pro;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
