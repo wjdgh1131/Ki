3a514f3dfef61e611719f0278f0be787050750b0
wlyoung89 <wndehdls@naver.com>
2015-05-06 17:55:04 +0900

    7th

--- app/src/main/java/com/hb/ki_pro/CmtAddActivity.java
+++ app/src/main/java/com/hb/ki_pro/CmtAddActivity.java
@@ -2,7 +2,9 @@ package com.hb.ki_pro;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,19 +19,38 @@ public class CmtAddActivity extends ActionBarActivity{
 
         insert_cmt_form = (EditText)findViewById(R.id.insert_cmt_form);
 
-        findViewById(R.id.cmt_cancel).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-             finish();
-            }
-        });
-        findViewById(R.id.cmt_ok).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), insert_cmt_form.getText().toString(), Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
+    }
 
+    protected void write_ok(){
+        Toast.makeText(getApplicationContext(), "코멘트 작성 완료: "+insert_cmt_form.getText().toString(), Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_sub, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_write:
+                write_ok();
+                return true;
+            case R.id.action_back:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
